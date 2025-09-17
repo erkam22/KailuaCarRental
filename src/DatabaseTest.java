5a88d5150b8c69254b84c0082326ddffbca5884c
erkam22 <erkamo3032@gmail.com>
2025-09-17 12:03:14 +0200

    Beskriv hvad du har ændret, fx 'Ryddet op i CRUD-metoder'

--- src/DatabaseTest.java
+++ src/DatabaseTest.java
@@ -5,14 +5,14 @@ import java.sql.SQLException;
 public class DatabaseTest {
     public static void main(String[] args) {
         try {
-            // Valgfrit i nyere Java, sikrer driveren findes
+
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // Forbind til MySQL-databasen
+
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/KailuaCarRental?serverTimezone=UTC",
-                    "root",          // Din MySQL-bruger
-                    "Politi123"      // Din MySQL-adgangskode
+                    "root",
+                    "Politi123"
             );
 
             System.out.println("Forbundet til database!");
